d1a7c899b221d8204b1d25b825aa19b3a1a24960
Carlos Diaz <cdiaz86@gmail.com>
2020-11-12 22:40:53 -0500

    Captures form input in state. Sends to prop.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function SearchForm() {
+function SearchForm(props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [numberOfGifs, setNumberOfGifs] = useState(0);
 
-  function logContents(event) {
-    console.log(event.target.value);
+  function SetUserInput(event) {
+    if (event.target.name === 'query') setSearchTerm(event.target.value);
+
+    if (event.target.name === 'number') setNumberOfGifs(event.target.value);
   }
+
+  function gatherInput(event) {
+    event.preventDefault();
+    props.userInput(searchTerm, numberOfGifs);
+  }
+
   return (
-    <form action="">
+    <form action="" onSubmit={gatherInput}>
       <label htmlFor="query">Search Term:</label>
-      <input type="text" id="query" name="query" onChange={logContents}></input>
+      <input type="text" id="query" name="query" onChange={SetUserInput}></input>
 
       <label htmlFor="query">Number Requested:</label>
-      <input type="text" id="number" name="number" onChange={logContents}></input>
+      <input type="text" id="number" name="number" onChange={SetUserInput}></input>
       <input type="submit" value="Submit"></input>
 
     </form> 
