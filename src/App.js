82c6f9f4aa4625ee8f207572a4647f90dcd4e6ce
Carlos Diaz <cdiaz86@gmail.com>
2020-11-12 20:35:15 -0500

    Makes API call. Handles error.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+  async function gifSearch() {
+    try {
+      const url = 'http://api.giphy.com/v1/gifs/search?q=cats&api_key=JCi4z2AIWowzJTWX7HtbX7OjQIeZEHpT&limit=5'
+      const response = await fetch(url, {mode: 'cors' });
+      const data = await response.json();
+      console.log(data);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  gifSearch();
   return (
     <div className="App">
       hi
