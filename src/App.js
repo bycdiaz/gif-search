8ab4a18740e1fae75c9735dad606d303271cd559
Carlos Diaz <cdiaz86@gmail.com>
2020-11-13 19:04:59 -0500

    Improves variable name.

--- src/App.js
+++ src/App.js
@@ -14,20 +14,21 @@ function App() {
 
       const response = await fetch(url, { mode: 'cors' });
       const data = await response.json();
+      // TODO return result so function call below isn't necessary
       setSearchResults(data.data);
     } catch(error) {
       console.log(error);
     }
   }
 
-  function userInput(searchTerm, numberRequested) {
+  function onUserInput(searchTerm, numberRequested) {
     gifSearch(searchTerm, numberRequested);
   }
 
   return (
     <div className="App">
       <h1>Gif Search</h1>
-      <SearchForm userInput={userInput} />
+      <SearchForm onUserInput={onUserInput} />
       {/* TODO Bring in React Router. Clicking on result can take to Gif info page. */}
       {/* TODO Set up ability to up or downvote Gif in info page */}
       {/* TODO If Gif is rated in Gif info page, persist Gif details and rating in local storage. */}
