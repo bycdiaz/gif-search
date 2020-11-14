f6a9c2d253bcbe0ef1cdf86f7570df55eea83e1f
Carlos Diaz <cdiaz86@gmail.com>
2020-11-13 19:04:46 -0500

    Destructures props.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 
+// TODO Disable inputs until successful response.
+// TODO Show pending action.
 // TODO Debounce user input
 
-function SearchForm(props) {
+function SearchForm({ onUserInput }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [numberOfGifs, setNumberOfGifs] = useState(0);
 
-  function SetUserInput(event) {
+  function setUserInput(event) {
     // TODO lowercase and trim
     if (event.target.name === 'query') setSearchTerm(event.target.value);
 
@@ -16,17 +18,17 @@ function SearchForm(props) {
 
   function gatherInput(event) {
     event.preventDefault();
-    props.userInput(searchTerm, numberOfGifs);
+    onUserInput(searchTerm, numberOfGifs);
   }
 
   return (
     <form action="" onSubmit={gatherInput}>
 
       <label htmlFor="query">Search Term:</label>
-      <input type="text" id="query" name="query" onChange={SetUserInput}></input>
+      <input type="text" id="query" name="query" onChange={setUserInput}></input>
 
       <label htmlFor="query">Number Requested:</label>
-      <input type="text" id="number" name="number" onChange={SetUserInput}></input>
+      <input type="text" id="number" name="number" onChange={setUserInput}></input>
 
       <input type="submit" value="Submit"></input>
 
